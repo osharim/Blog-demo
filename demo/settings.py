e9ff211f83239c56535556e6d78d2aa0da63e524
omar <omarisel01@gmail.com>
2012-09-19 00:05:41 -0500

    changin data

--- demo/settings.py
+++ demo/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'bindsmec_omr',
         'PASSWORD': 'nf9ckpg',
-        'HOST': 'just96.justhost.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
