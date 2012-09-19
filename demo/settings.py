5d20f7e23b8f39069295a6b992dcf1d237df8f8d
omar <omarisel01@gmail.com>
2012-09-18 23:52:41 -0500

    changin data

--- demo/settings.py
+++ demo/settings.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'demo_django',                      # Or path to database file if using sqlite3.
+        'NAME': 'bindsmec_django',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'bindsmec_omr',
+        'PASSWORD': 'nf9ckpg',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
